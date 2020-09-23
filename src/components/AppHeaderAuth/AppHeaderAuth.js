f7cf47fa48a687ca8c89b01901948cccb2c50818
Matt Savarino <matt@mattsavarino.com>
2020-09-23 10:58:30 -0700

    updated header auth styles

--- src/components/AppHeaderAuth/AppHeaderAuth.js
+++ src/components/AppHeaderAuth/AppHeaderAuth.js
@@ -11,15 +11,14 @@ export default function AppHeaderAuth() {
   const authElement = document.querySelector('.auth');
   if (authElement) {
     // Default sign in
-    let html = 'Welcome | <a href="/login">Sign In</a>';
+    let html = '<a href="/login" className="nav-link">Sign In</a>';
     
     // User profile and sign out
     let clientPrincipal = (user && user.clientPrincipal) || null,
         userDetails     = (clientPrincipal && clientPrincipal.userDetails) || null;
 
     if (userDetails) {
-      html = `Hi <a href="/profile">${userDetails}</a>`;
-      html += ' | <a href="/logout">Sign Out</a>';
+      html = `${userDetails} | <a href="/logout" className="nav-link">Sign Out</a>`;
     }
 
     authElement.innerHTML = html;
