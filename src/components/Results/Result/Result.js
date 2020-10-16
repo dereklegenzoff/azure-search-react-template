cd9c8bca1bcb5dbf42fd3478c722935219376687
Matt Savarino <matt@mattsavarino.com>
2020-10-16 11:16:50 -0700

    updated jsx styles to css

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -2,32 +2,12 @@ import React from 'react';
 
 import './Result.css';
 
-
 export default function Result(props) {
-
-    const cardStyle = {
-        width: '10rem',
-        maxHeight: '18rem'
-        
-    };
-    
-    const imageStyle = {
-        minWidth: '6em',
-        maxWidth: '10em',
-        maxHeight: '12em',
-        height: 'auto',
-        vertialAlign: 'center'
-    };
-    
-    const bodyStyle = {
-        padding: '0.25rem'
-    };
-
     return(
-    <div className="card result" style={cardStyle}>
+    <div className="card result">
         <a href={`/details/${props.document.id}`}>
-            <img className="card-img-top" src={props.document.image_url} style={imageStyle} alt="book cover"></img>
-            <div className="card-body" style={bodyStyle}>
+            <img className="card-img-top" src={props.document.image_url} alt={props.document.original_title}></img>
+            <div className="card-body">
                 <h6 className="title-style">{props.document.original_title}</h6>
             </div>
         </a>
